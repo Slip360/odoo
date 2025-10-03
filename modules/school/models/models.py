e8ef9e3930397c62490e2de394aa32c3d5135b75
Slip360 <josecatun2015@hotmail.com>
2025-10-02 22:16:08 -0600

    Módulo de estudiantes

---
+++ modules/school/models/models.py
@@ -0,0 +1,7 @@
+from odoo import models, fields
+
+class Student(models.Model):
+    _name = "school.student"
+    _description = "Tabla de estudiantes"
+    name = fields.Char(string="Name", required=True)
+    age = fields.Integer(string="Age", required=True)
